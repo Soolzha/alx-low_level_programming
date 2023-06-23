ecd9f96a392f2c7cc80ca254d68945211e71bc54
Soolzha <souhail.16700@gmail.com>
2023-06-22 17:31:29 -0700

    Taskz5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,22 +6,16 @@
  */
 void more_numbers(void)
 {
-	char n, c;
-	int i = 0;
+ int i, j;
 
-	while (i < 10)
-	{
-		for (n = 0; n <= 14; n++)
-		{
-			c = n;
-			if (n > 9)
-			{
-				_putchar('1');
-				c = n % 10;
-			}
-			_putchar('0' + c);
-		}
-		_putchar('0' + c);
-		i++;
-	}
+ for (i = 0; i < 10; i++)
+ {
+ for (j = 0; j < 15; j++)
+ {
+ if (j >= 10)
+ _putchar(j / 10 + '0');
+ _putchar(j % 10 + '0');
+ }
+ _putchar('\n');
+ }
 }
